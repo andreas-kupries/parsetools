6d6bfd07ee3271510bca58f9734e3f64d8363550
Andreas Kupries <akupries@shaw.ca>
2012-11-08 22:58:05 +0000

    Char quoting operations

--- quoting/quote.tcl
+++ quoting/quote.tcl
@@ -1,36 +1,64 @@
 # -*- tcl -*-
 #
-# Copyright (c) 2009 by Andreas Kupries <andreas_kupries@users.sourceforge.net>
-# Operations with characters: (Un)quoting.
+# Copyright (c) 2009-2012 by Andreas Kupries <andreas_kupries@users.sourceforge.net>
+# Various (un)quoting operations on characters.
+
+# XXX TODO: Documentation. Tests.
 
 # ### ### ### ######### ######### #########
 ## Requisites
 
 package require Tcl 8.5
 
-namespace eval char {
-    namespace export unquote quote
+namespace eval quote {
+    namespace export unquote tcl string comment cstring
     namespace ensemble create
-    namespace eval quote {
-	namespace export tcl string comment cstring
-	namespace ensemble create
-    }
 }
 
 # ### ### ### ######### ######### #########
 ## API
 
-proc ::char::unquote {args} {
+proc ::quote::unquote {args} {
     if {1 == [llength $args]} { return [Unquote {*}$args] }
     set res {}
     foreach ch $args { lappend res [Unquote $ch] }
     return $res
 }
 
-proc ::char::Unquote {ch} {
+proc ::quote::tcl {args} {
+    if {1 == [llength $args]} { return [Tcl {*}$args] }
+    set res {}
+    foreach ch $args { lappend res [Tcl $ch] }
+    return $res
+}
 
-    # A character, stored in quoted form is transformed back into a
-    # proper Tcl character (i.e. the internal representation).
+proc ::quote::string {args} {
+    if {1 == [llength $args]} { return [String {*}$args] }
+    set res {}
+    foreach ch $args { lappend res [String $ch] }
+    return $res
+}
+
+proc ::quote::cstring {args} {
+    if {1 == [llength $args]} { return [CString {*}$args] }
+    set res {}
+    foreach ch $args { lappend res [CString $ch] }
+    return $res
+}
+
+proc ::quote::comment {args} {
+    if {1 == [llength $args]} { return [Comment {*}$args] }
+    set res {}
+    foreach ch $args { lappend res [Comment $ch] }
+    return $res
+}
+
+# ### ### ### ######### ######### #########
+## Internal commands.
+
+proc ::quote::Unquote {ch} {
+    # A character, stored in (tcl) quoted form is transformed back
+    # into a proper Tcl character (i.e. the internal representation).
 
     switch -exact -- $ch {
 	"\\n"  {return \n}
@@ -57,17 +85,10 @@ proc ::char::Unquote {ch} {
     return $ch
 }
 
-proc ::char::quote::tcl {args} {
-    if {1 == [llength $args]} { return [Tcl {*}$args] }
-    set res {}
-    foreach ch $args { lappend res [Tcl $ch] }
-    return $res
-}
-
-proc ::char::quote::Tcl {ch} {
+proc ::quote::Tcl {ch} {
     # Converts a Tcl character (internal representation) into a string
     # which is accepted by the Tcl parser, will regenerate the
-    # character in question and is 7bit ASCII.
+    # character in question and is portable (7bit ASCII).
 
     # Special characters
 
@@ -96,32 +117,23 @@ proc ::char::quote::Tcl {ch} {
     }
 
     # Beyond 7-bit ASCII: Unicode
-
     if {$chcode > 127} {
 	return \\u[format %04x $chcode]
     }
 
     # Regular character: Is its own representation.
-
     return $ch
 }
 
-proc ::char::quote::string {args} {
-    if {1 == [llength $args]} { return [String {*}$args] }
-    set res {}
-    foreach ch $args { lappend res [String $ch] }
-    return $res
-}
-
-proc ::char::quote::String {ch} {
+proc ::quote::String {ch} {
     # Converts a Tcl character (internal representation) into a string
     # which is accepted by the Tcl parser and will generate a human
     # readable representation of the character in question, one which
     # when written to a channel (via puts) describes the character
-    # without using any unprintable characters. It may use backslash-
-    # quoting. High utf characters are quoted to avoid problems with
-    # the still prevalent ascii terminals. It is assumed that the
-    # string will be used in a ""-quoted environment.
+    # without using any unprintable (non-portable) characters. It may
+    # use backslash-quoting. High utf characters are quoted to avoid
+    # problems with the still prevalent ascii terminals. It is assumed
+    # that the string will be used in a ""-quoted environment.
 
     # Special characters
 
@@ -146,32 +158,23 @@ proc ::char::quote::String {ch} {
     }
 
     # Beyond 7-bit ASCII: Unicode
-
     if {$chcode > 127} {
 	return \\\\u[format %04x $chcode]
     }
 
     # Regular character: Is its own representation.
-
     return $ch
 }
 
-proc ::char::quote::cstring {args} {
-    if {1 == [llength $args]} { return [CString {*}$args] }
-    set res {}
-    foreach ch $args { lappend res [CString $ch] }
-    return $res
-}
-
-proc ::char::quote::CString {ch} {
+proc ::quote::CString {ch} {
     # Converts a Tcl character (internal representation) into a string
-    # which is accepted by the Tcl parser and will generate a human
+    # which is accepted by a C compiler and will generate a human
     # readable representation of the character in question, one which
     # when written to a channel (via puts) describes the character
-    # without using any unprintable characters. It may use backslash-
-    # quoting. High utf characters are quoted to avoid problems with
-    # the still prevalent ascii terminals. It is assumed that the
-    # string will be used in a ""-quoted environment.
+    # without using any unprintable (non-portable) characters. It may
+    # use backslash-quoting. High utf characters are quoted to avoid
+    # problems with the still prevalent ascii terminals. It is assumed
+    # that the string will be used in a ""-quoted environment.
 
     # Special characters
 
@@ -192,24 +195,15 @@ proc ::char::quote::CString {ch} {
     }
 
     # Beyond 7-bit ASCII: Unicode
-
     if {$chcode > 127} {
 	return \\\\u[format %04x $chcode]
     }
 
     # Regular character: Is its own representation.
-
     return $ch
 }
 
-proc ::char::quote::comment {args} {
-    if {1 == [llength $args]} { return [Comment {*}$args] }
-    set res {}
-    foreach ch $args { lappend res [Comment $ch] }
-    return $res
-}
-
-proc ::char::quote::Comment {ch} {
+proc ::quote::Comment {ch} {
     # Converts a Tcl character (internal representation) into a string
     # which is accepted by the Tcl parser when used within a Tcl
     # comment.
@@ -236,18 +230,16 @@ proc ::char::quote::Comment {ch} {
     }
 
     # Beyond 7-bit ASCII: Unicode
-
     if {$chcode > 127} {
 	return \\u[format %04x $chcode]
     }
 
     # Regular character: Is its own representation.
-
     return $ch
 }
 
 # ### ### ### ######### ######### #########
 ## Ready
 
-package provide char 1
+package provide quote 1
 
